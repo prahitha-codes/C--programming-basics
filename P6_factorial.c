4e5a3fd3fc0360ca5a375d1dbf80d87c28acd94f
prahitha-codes <prahitha2007@gmail.com>
2026-02-01 09:02:00 +0530

    Create P6_factorial_of_number.c

---
+++ P6_factorial.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+  int n,fact=1,i;
+  printf("Enter a number;");
+  scanf("%d",&n);
+  for(i=1;i<=n;i++)
+    fact=fact*i;
+  printf("Factorial of %d =%d",n,fact);
+  return 0;
+}
